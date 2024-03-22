e3cfb98cf05fdfc72579a48d302f90083e41cc5a
Shubham Puhal <shubhampuhal01@gmail.com>
2024-03-22 08:18:30 +0530

    Access granted to front-end

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ dotenv.config();
 import snippetRouter from "./Routers/index.js"
 
 const app=express();
-const liveLink = 'https://savesnippet.onrender.com';
+const backendliveLink = 'https://savesnippet.onrender.com';
+const frontendLiveLink = 'https://save-snippet.web.app/';
 app.use(cors({
   origin: function (origin, callback) {
-    const allowedOrigins = ['http://localhost:5173', 'http://localhost:8080', liveLink];
+    const allowedOrigins = ['http://localhost:5173', 'http://localhost:8080', backendliveLink, frontendLiveLink];
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
@@ -31,7 +32,7 @@ app.use("/api/snippet" , snippetRouter)
 const startServer=async()=>{
   try{
       connectDB(process.env.MONGO_URL);
-      app.listen(8080, ()=>console.log("Server started on ",liveLink));
+      app.listen(8080, ()=>console.log("Server started on ",backendliveLink));
   } catch(error){
       console.log(error);
   }
